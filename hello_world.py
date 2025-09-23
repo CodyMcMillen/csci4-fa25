aca20803215e79c7459453aaa0452ba401ca586d
CodyMcMillen <c50214267@gmail.com>
2025-09-23 17:42:13 +0000

    hello there

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
+print("hello World")
+
+import array as arr
+
+a = arr.array(
+    'h', # opcode for unsigned short 
+    [26] # list
+)
+
+b = arr.array('l', [1, 2, 3, 4, 5])
+
+print(a.pop())
+
+print(b.pop())
 
