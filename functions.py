ef84fb54a727a007ee2cde5ad1697f9a8f6ea64c
CodyMcMillen <c50214267@gmail.com>
2025-09-28 22:00:11 +0000

    chicken_nuggets

---
+++ functions.py
@@ -0,0 +1,15 @@
+#python
+
+def f(x):
+    return (x+10)
+
+print(f(4), f(5), f(6))
+
+def f(x):
+    return (x-2)
+
+print(f(2), f(4), f(6))
+
+chicken_nuggets = [4, 8, 12]
+
+print(sum(chicken_nuggets))
